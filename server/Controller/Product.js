787cac038fbfe1fcd8ea0812b8bb5c011072a553
Pravu-Ranjan <Pravu_ps7@outlook.com>
2021-07-09 00:49:21 +0530

    minor update

--- server/Controller/Product.js
+++ server/Controller/Product.js
@@ -1,6 +1,6 @@
-const Product = require("../Model/Product");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
+const Product = require('../Model/Product')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 exports.create = async (req, res) => {
   try {
@@ -12,51 +12,52 @@ exports.create = async (req, res) => {
       customer: req.body.customer,
       price: req.body.price,
       status: req.body.status,
-    });
-    return res.status(200).send(products);
+    })
+    return res.status(200).send(products)
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error)
   }
-};
+}
 
 exports.findAll = async (req, res) => {
   try {
-    let products = await Product.findAll({});
-    let searchKeyword = req.query.s;
-        
-    if(!searchKeyword) {
-      return res.status(200).send(products);
-    }else {
-      searchKeyword = searchKeyword.toLowerCase();
+    let products = await Product.findAll({})
+    let searchKeyword = req.query.s
+
+    if (!searchKeyword) {
+      return res.status(200).send(products)
+    } else {
+      searchKeyword = searchKeyword.toLowerCase()
       products = await Product.findAll({
-        where : {[Op.or] : {
-          service:  { [Op.like]: '%' + searchKeyword + '%' },
-          assign : { [Op.like]: '%' + searchKeyword + '%' }
-        }}
-      });
-      return res.status(200).send(products);
+        where: {
+          [Op.or]: {
+            service: { [Op.like]: '%' + searchKeyword + '%' },
+            assign: { [Op.like]: '%' + searchKeyword + '%' },
+          },
+        },
+      })
+      return res.status(200).send(products)
     }
-   
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error)
   }
-};
+}
 
 exports.findOne = async (req, res) => {
   try {
-    let id = req.params.id;
+    let id = req.params.id
     let product = await Product.findOne({
       where: { bookingID: id },
-    });
-    return res.status(200).send(product);
+    })
+    return res.status(200).send(product)
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(400).send(error)
   }
-};
+}
 
 exports.update = async (req, res) => {
   try {
-    let id = req.params.id;
+    let id = req.params.id
     let product = await Product.update(
       {
         service: req.body.service,
@@ -69,24 +70,24 @@ exports.update = async (req, res) => {
       {
         where: { bookingID: id },
       }
-    );
-    return res.status(200).send("Data got updated!!!");
+    )
+    return res.status(200).send('Data got updated!!!')
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(400).send(error)
   }
-};
+}
 
 exports.delete = async (req, res) => {
   try {
-    let id = req.params.id;
+    let id = req.params.id
     let product = await Product.destroy({
       where: { bookingID: id },
-    });
+    })
 
     return res.status(200).send({
-      message: "Data deleted!!!",
-    });
+      message: 'Data deleted!!!',
+    })
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(400).send(error)
   }
-};
+}
--- server/Server.js
+++ server/Server.js
@@ -1,20 +1,20 @@
-const app = require("./App");
-const db = require("./Database");
-const dotenv = require("dotenv");
-dotenv.config();
+const app = require('./App')
+const db = require('./Database')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const PORT = process.env.SERVER_PORT || 4002;
+const PORT = process.env.SERVER_PORT || 4002
 
 const DBConnection = async () => {
   try {
-    await db.authenticate();
-    console.log("Database connection has been established successfully.");
-    await db.sync({ force: false });
-    console.log("All models were synchronized successfully.");
+    await db.authenticate()
+    console.log('Database connection has been established successfully.')
+    await db.sync({ force: false })
+    console.log('All models were synchronized successfully.')
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error)
   }
-};
+}
 app.listen(PORT, DBConnection(), () =>
   console.log(`Server up and running on port: ${PORT}`)
-);
+)
